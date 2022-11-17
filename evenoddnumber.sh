dc1fe783734f475934d06e86adc1cf157178cec0
Jeff Holcomb <jeffholcombcloud@gmail.com>
2022-11-17 10:50:19 -0600

    modified script

--- evenoddnumber.sh
+++ evenoddnumber.sh
@@ -4,6 +4,7 @@
 #Authors: Sarah B, Jeff H, Neethu V, Kra K, Durga G
 #Date 11/17/2022
 #Specials instructions: N/A
+#Source: https://www.hostinger.com/tutorials/bash-script-example#18_Read_Files
 read -p "Enter a number and I will check if its odd or even " mynumber
 #If the number divided by 2 has a remainder of 0 then do this, otherwise do this
 if [ $((mynumber%2)) -eq 0 ]
